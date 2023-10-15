47c6b06cd3f02fd4c01cf4155ef9a6290409b3ee
parth thakor <parththakor2610@gmail.com>
2023-10-15 11:04:34 +0530

    change lable to label

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -2,17 +2,17 @@ import Chart from "../Chart/Charts";
 
 const ExpensesChart = (props)=>{
   const chartDtataPoint = [
-  {lable : "Jan" , value : 0},
-  {lable : "Feb" , value : 0},
-  {lable : "Mar" , value : 0},
-  {lable : "May" , value : 0},
-  {lable : "Jun" , value : 0},
-  {lable : "Jul" , value : 0},
-  {lable : "Aug" , value : 0},
-  {lable : "Sep" , value : 0},
-  {lable : "Oct" , value : 0},
-  {lable : "Nov" , value : 0},
-  {lable : "Dec" , value : 0},
+  {label : "Jan" , value : 0},
+  {label : "Feb" , value : 0},
+  {label : "Mar" , value : 0},
+  {label : "May" , value : 0},
+  {label : "Jun" , value : 0},
+  {label : "Jul" , value : 0},
+  {label : "Aug" , value : 0},
+  {label : "Sep" , value : 0},
+  {label : "Oct" , value : 0},
+  {label : "Nov" , value : 0},
+  {label : "Dec" , value : 0},
 ];
 for(const expense of props.expenses){
   const expenseMonth = expense.date.getMonth(); 
